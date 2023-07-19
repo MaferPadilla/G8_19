932831f3089ccaa6fc11474c7dda6106b1cad4f3
Roxana-Garcia <rygarciaa@unah.hn>
2023-07-18 22:22:59 -0600

    API FACTURA

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,7 @@
 "use strict";
 var  ProductoController = require("../controllers/producto-controller"),
+     FacturaController = require("../controllers/factura-controller"),
+     ClienteController = require("../controllers/cliente-controller"),
   express = require("express"),
   router = express.Router();
 
@@ -9,6 +11,21 @@ router
   .post("/producto/insertar/:id_producto", ProductoController.post)
   .put("/producto/actualizar/:id_producto", ProductoController.put)
   .delete("/producto/eliminar/:id_producto", ProductoController.delete)
-  //.use(ProductoController.console.error404);
 
+  .get("/factura/getall", FacturaController.getAll)
+  .get("/factura/getone/:numero_factura", FacturaController.getOne)
+  .post("/factura/insertar/:numero_factura", FacturaController.post)
+  .put("/factura/actualizar/:numero_factura", FacturaController.put)
+  .delete("/factura/eliminar/:numero_factura", FacturaController.delete)
+
+  .get("/cliente/getAll", ClienteController.getAll)
+  .post("/cliente/getOne/:no_cliente", ClienteController.getOne)
+  .post("/cliente/insertar/", ClienteController.post)
+  .put("/cliente/actualizar/", ClienteController.put)
+  .delete("/cliente/eliminar/", ClienteController.delete)
+
+  .use(ClienteController.error404)
+  .use(ProductoController.error404)
+  .use(FacturaController.error404);
+  
 module.exports = router;
